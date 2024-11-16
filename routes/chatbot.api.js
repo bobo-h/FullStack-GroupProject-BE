8eac40abdfabc8c794ff60fc7666436063fadfe6
subeenDo <subeen2318@gmail.com>
2024-11-16 19:33:00 +0900

    feat : ai printLine controller

--- routes/chatbot.api.js
+++ routes/chatbot.api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const chabotController = require("../controllers/chatbot.controller");
 
 router.post("/", chabotController.createChatbot);
+router.post("/printLine", chabotController.createPrintLine)
 
 
 module.exports=router;
