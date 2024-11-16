7bf31ef008d41fe4a3401480fbf03cb49431f467
scKim <lansco1229@gmail.com>
2024-11-16 19:52:06 +0900

    feat: add a payment function

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const authApi = require("./auth.api");
 const diaryApi = require("./diary.api");
 const chatbotApi = require("./chatbot.api");
 const productApi = require("./product.api")
+const orderApi = require("./order.api")
 
 router.use("/user", userApi);
 router.use("/auth", authApi);
 router.use("/diary", diaryApi);
 router.use("/chatbot",chatbotApi);
 router.use("/product", productApi)
+router.use("/order", orderApi)
 
 module.exports = router;
