f191b41035ae36757c92c00b2dce640674f7422a
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-11-16 19:46:53 +0900

    feat: add createDiary and getDiaryList

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
+const allowedOrigins = [
+  "https://shopping-mall-1st.netlify.app",
+  "http://localhost:3000",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-require("dotenv").config();
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.json());
+
 app.use("/api", indexRouter);
 
-
-const mongoURI = process.env.MONGODB_URI_PROD;
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
 
 mongoose
   .connect(mongoURI)
   .then(() => console.log("Mongoose connected"))
   .catch((err) => console.log("DB connection fail", err));
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is on");
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server running on port ${process.env.PORT || 8080}`);
 });
--- controllers/diary.controller.js
+++ controllers/diary.controller.js
@@ -1,6 +1,86 @@
+const Diary = require("../models/Diary");
+
 const diaryController = {};
 
 diaryController.createDiary = async (req, res) => {
   try {
-  } catch (error) {}
+    const { userId, title, content, image, selectedDate, mood } = req.body;
+    if (!userId || !title || !content || !image || !selectedDate || !mood) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+    const newDiary = new Diary({
+      userId,
+      title,
+      content,
+      image,
+      selectedDate,
+      mood,
+    });
+    await newDiary.save();
+
+    res.status(200).json({
+      status: "success",
+      diary: newDiary,
+    });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
 };
+
+diaryController.getDiaryList = async (req, res) => {
+  try {
+    const { userId } = req.query;
+
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required." });
+    }
+
+    const diaries = await Diary.aggregate([
+      { $match: { userId: mongoose.Types.ObjectId(userId) } },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$selectedDate" },
+            month: { $month: "$selectedDate" },
+          },
+          diaries: {
+            $push: {
+              id: "$_id",
+              title: "$title",
+              content: "$content",
+              image: "$image",
+              selectedDate: "$selectedDate",
+              mood: "$mood",
+              isEdited: "$isEdited",
+              createdAt: "$createdAt",
+              updatedAt: "$updatedAt",
+            },
+          },
+        },
+      },
+      { $sort: { "_id.year": -1, "_id.month": -1 } },
+      {
+        $project: {
+          _id: 0,
+          yearMonth: {
+            $concat: [
+              { $toString: "$_id.year" },
+              "-",
+              { $toString: "$_id.month" },
+            ],
+          },
+          diaries: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: diaries,
+    });
+  } catch (error) {
+    res.status(500).json({ status: "fail", error: error.message });
+  }
+};
+
+module.exports = diaryController;
