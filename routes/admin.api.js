55c7a447c5994740f38b393cce49867bbc42f1b0
Dahee Park <dahee5654@gmail.com>
2024-11-18 16:31:24 +0900

    docs : add import admin controller

--- routes/admin.api.js
+++ routes/admin.api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const adminController = require("../controllers/admin.controller");
 
 // 회원 리스트 가져오기
 router.get("/AllUser", adminController.getAllUser);
